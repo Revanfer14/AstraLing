8c951b93cf22ab2fe9ad1731fc7a3a1ffc8d920d
Revanfer14 <revancrozz@gmail.com>
2026-06-14 16:29:46 +0700

    first commit

---
+++ AstraLing/AstraLingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstraLingApp.swift
+//  AstraLing
+//
+//  Created by Revan Ferdinand on 14/06/26.
+//
+
+import SwiftUI
+
+@main
+struct AstraLingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
